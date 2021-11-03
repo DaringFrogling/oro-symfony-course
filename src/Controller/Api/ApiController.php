7a2542149e8f5a5cc3cb953ca2929a19aadbb314
Dmytro Lysiakin <daringfrogling@gmail.com>
2021-11-03 21:07:10 +0200

    api endpoints implemented

---
+++ src/Controller/Api/ApiController.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Component\HttpFoundation\{Request, Response};
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController
+{
+    /**
+     * Returns information about client.
+     *
+     * @Route("/api/my-info", methods="GET", name="client_info")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function actionClientInfo(Request $request): Response
+    {
+        if (!$request->headers->get('Content-type: application/json')) {
+            return new Response(null, 415);
+        }
+
+        $clientInfo = [
+            'ip' => $request->getClientIps()[0],
+            'language' => $request->getLanguages(),
+            'browser' => ($request->headers->get('User-Agent'))
+        ];
+        $responseData = json_encode($clientInfo);
+
+        file_put_contents('logs.txt', $responseData . PHP_EOL, FILE_APPEND | LOCK_EX);
+
+        return new Response(
+            $responseData,
+            Response::HTTP_OK,
+            [
+                'Content-type' => 'application/json'
+            ]
+        );
+    }
+
+    /**
+     * Returns all resources of application.
+     *
+     * @Route("/api", methods="GET", name="list_of_resources")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function actionIndex(Request $request): Response
+    {
+        if (!$request->headers->get('Content-type: application/json')) {
+            return new Response(null, 415);
+        }
+
+        $data = $this->getDataFromFile();
+        $responseData = $this->prepareData($request, $data);
+
+        return new Response(
+            json_encode($responseData),
+            Response::HTTP_OK,
+            [
+                'Content-type' => 'application/json'
+            ]
+        );
+    }
+
+    /**
+     * Returns all resources of application.
+     *
+     * @Route("/api/{product}", methods="GET", name="product_id")
+     *
+     * @param Request $request
+     * @param string $product
+     *
+     * @return Response
+     */
+    public function actionProduct(Request $request, string $product): Response
+    {
+        if (!$request->headers->get('Content-type: application/json')) {
+            return new Response(null, 415);
+        }
+
+        $data = $this->getDataFromFile();
+        $uris = $this->prepareData($request, $data);
+        $neededProducts = array_filter($uris, function ($item) use ($product) {
+            return str_contains($item, $product);
+        });
+
+        if (empty($neededProducts)) {
+            throw new NotFoundHttpException();
+        }
+
+        $neededProduct = $data[key($neededProducts)];
+
+        return new Response(
+            json_encode($neededProduct),
+            Response::HTTP_OK,
+            [
+                'Content-type' => 'application/json'
+            ]
+        );
+    }
+
+    /**
+     * Gets data from file without header line.
+     *
+     * @return array
+     */
+    private function getDataFromFile(): array
+    {
+        $products = array_map('str_getcsv', file('./products.csv'));
+        array_shift($products);
+
+        return $products;
+    }
+
+    /**
+     * Returns formatted list of resources.
+     *
+     * @param Request $request
+     * @param array $data
+     *
+     * @return array
+     */
+    private function prepareData(Request $request, array $data): array
+    {
+        $responseData = [];
+
+        foreach ($data as $product) {
+            $productUri = $this->uriFormatter($product[0]);
+            $responseData[] = $request->getSchemeAndHttpHost() . "/{$productUri}";
+        }
+
+        return $responseData;
+    }
+
+    /**
+     * Formats product URI.
+     *
+     * @param string $product
+     *
+     * @return string
+     */
+    private function uriFormatter(string $product): string
+    {
+        $positionToEraseStart = mb_strpos($product, '_');
+        $productName = mb_substr($product, 0, $positionToEraseStart);
+        $productIdSubStr = str_split(mb_substr($product, $positionToEraseStart));
+        $productId = [];
+
+        foreach ($productIdSubStr as $symbol) {
+            if ((int)$symbol >= 1) {
+                $productId[] = $symbol;
+            } elseif (!empty($productId)) {
+                $productId[] = $symbol;
+            }
+        }
+
+        return $productName . implode($productId);
+    }
+}
